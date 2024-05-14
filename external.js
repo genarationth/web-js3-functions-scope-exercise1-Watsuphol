60d6d8a5b423113eb02c58627cb9c8a4a5746fa1
Watsuphol <wasumc.design@gmail.com>
2024-05-14 14:07:02 +0700

    added external

---
+++ external.js
@@ -0,0 +1,37 @@
+// ## Exercise 1.1 Create function following below :</br>
+
+// add(a, b)</br>
+// subtract(a, b)</br>
+// multiply(a, b)</br>
+// divide(a, b)</br>
+
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+console.log(add(4,5));
+console.log(subtract(9,5));
+console.log(multiply(4,5));
+console.log(divide(4,2));
+
+// ## Exercise 1.2 Create Function Inform---</br>
+
+function inform(firstName, location, hobby) {
+    return  "Hi, my name is " + firstName + " live in " + location + " and enjoy "+hobby;
+}
+
+console.log(inform("mc","bkk","drawing"));
+
+// console.log("Hi, my name is " + firstName + "I live in " + location + "and enjoy "+hobby);
